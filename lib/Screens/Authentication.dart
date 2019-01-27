fc74d205319bdbc5ed35484e3c448a5bb9b68bf6
Bastien Falcou <bastien@fueled.com>
2019-01-27 17:25:38 -0500

    feat(authentication): addition multiple files

--- lib/Screens/Authentication.dart
+++ lib/Screens/Authentication.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_test/Screens/RandomWords.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc_test/Blocs/AuthenticationBloc.dart';
 
 class Authentication extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final AuthenticationBloc authenticationBloc = BlocProvider.of<AuthenticationBloc>(context);
+
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Authentication'),
